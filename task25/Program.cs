bc2f0536fbd4e4d1a1dd71e8ee1fbc583cc92afd
Elena Gerasimovich <nararock@mail.ru>
2022-09-27 00:33:51 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную степень B.
+exponenta();
+
+void exponenta(){
+    while(true){
+        Console.WriteLine("Введите число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите степень числа");
+        int degree = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"{number} в степени {degree} равняется {Math.Pow(number, degree)}");
+        Console.WriteLine("Продолжить возводить в степень? (Да или Нет)");
+        string answer = Console.ReadLine();
+        if (answer.ToLower() == "нет") break;
+    }
+}
