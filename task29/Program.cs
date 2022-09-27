bb870c58f46b1136721b26a85f34c3e0b97bffca
Elena Gerasimovich <nararock@mail.ru>
2022-09-27 23:39:06 +0300

    change enter

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,12 +1,17 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
 Console.WriteLine("Введите размер массива");
 int number = Convert.ToInt32(Console.ReadLine());
-outputArray(createArray(number));
+Console.WriteLine("Введите нижнюю границу для чисел в массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите верхнюю границу для чисел в массиве");
+int max = Convert.ToInt32(Console.ReadLine());
 
-int[] createArray(int size){
+outputArray(createArray(number, min, max));
+
+int[] createArray(int size, int minNumber, int maxNumber){
     int[] array = new int[size];
     for (int i = 0; i < size; i++){
-        array[i] = new Random().Next(0, 101);
+        array[i] = new Random().Next(minNumber, maxNumber + 1);
     }
     return array;
 }
