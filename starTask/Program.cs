a31e5b73e68d2e027246c0e179c92589050949dd
Elena Gerasimovich <nararock@mail.ru>
2022-09-27 23:42:40 +0300

    change method createArray()

--- starTask/Program.cs
+++ starTask/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Напишите программу, которая задаёт массив из 10 элементов, которые необходимо 
 //заполнить случайными значениями в диапазоне от -10 до 10
 // и найти максимальное значение среди них
-int[] myArray = createArray(10, -10, 11);
+int[] myArray = createArray(10, -10, 10);
 outputArray(myArray); 
 Console.WriteLine($"максимальное значение массива {myArray.Max()}");
 
 int[] createArray(int size, int min, int max){
     int[] array = new int[size];
     for (int i = 0; i < size; i++){
-        array[i] = new Random().Next(min, max);
+        array[i] = new Random().Next(min, max + 1);
     }
     return array;
 }
